55bffe7794f8c2f87f1e6626e92d960d76fe3743
Simon Cropp <simon.cropp@gmail.com>
2020-12-27 18:19:22 +1100

    Update DirectoryFilter.cs

--- src/MarkdownSnippets/Reading/DirectoryFilter.cs
+++ src/MarkdownSnippets/Reading/DirectoryFilter.cs
@@ -1,20 +1,4 @@
-﻿using System;
-
-namespace MarkdownSnippets
+﻿namespace MarkdownSnippets
 {
     public delegate bool DirectoryFilter(string directoryPath);
-
-    public class PathExclusions
-    {
-        public PathExclusions(
-            Func<string, bool> shouldExcludeFile,
-            Func<string, bool> shouldExcludeDirectory)
-        {
-            ShouldExcludeFile = shouldExcludeFile;
-            ShouldExcludeDirectory = shouldExcludeDirectory;
-        }
-
-        public Func<string,bool> ShouldExcludeFile { get; }
-        public Func<string,bool> ShouldExcludeDirectory{ get; }
-    }
 }
